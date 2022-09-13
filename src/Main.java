14d6a5a1eddfe3d357ad31cf277a3e9306e7659c
Andrei Grazhdan <myway@yandex-team.ru>
2022-09-13 17:48:39 +0700

    форматирование

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
                 "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit " +
                 "esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt " +
                 "in culpa qui officia deserunt mollit anim id est laborum.";
-WordsChecker wordsChecker = new WordsChecker(mytext);
-System.out.println(wordsChecker.hasWord("pariatur"));
+        WordsChecker wordsChecker = new WordsChecker(mytext);
+        System.out.println(wordsChecker.hasWord("pariatur"));
     }
 }
